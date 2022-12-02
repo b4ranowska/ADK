77a66e66fe3f28862d825dd61f57155d8b002900
Baranowska <marcelina.baranowska@student.wat.edu.pl>
2022-12-02 18:37:12 +0100

    wyświetlenie wykresu

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
 from application import app
 from flask import render_template, url_for
+from Signal import Signal
 import pandas as pd
 import json
 import plotly
@@ -8,12 +9,20 @@ import plotly.express as px
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='home')
 
 @app.route('/audio')
 def audio():
-    return render_template('audio.html')
+    return render_template('audio.html', title='audio')
 
 @app.route('/1plot')
 def plot1():
-    return render_template('1plot.html')
+
+    # Graph One
+    df = px.data.medals_wide()
+    fig1 = px.bar(df, x="nation", y=["gold", "silver", "bronze"], title="Wide-Form Input")
+    graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+    return render_template('1plot.html', title='plots', graph1JSON=graph1JSON)
+
